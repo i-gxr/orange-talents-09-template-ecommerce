79ce19a04a0bd825005f3edd5bb134b7477a02f3
Igor Silva <igor.pereira@zup.com.br>
2021-10-06 00:13:11 -0300

    Criação do projeto

---
+++ src/test/java/br/com/zup/mercado_livre/MercadoLivreApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.zup.mercado_livre;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MercadoLivreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
